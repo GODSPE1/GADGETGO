a7da09bc78f173ad31b6e3bf2a3df9771848fdd3
GODSPE1 <godspy18@gmail.com>
2024-09-17 00:33:41 +0100

    added Category Model class

--- app/models.py
+++ app/models.py
@@ -3,12 +3,16 @@ from flask_login import UserMixin
 
 # Product Model
 class Product(db.Model):
+    """Product Model class"""
+    
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(256))
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(1024), nullable=False)
     price = db.Column(db.Float, nullable=False)
     image = db.Column(db.String(255), nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref='products', lazy=True)
 
     def __repr__(self) -> str:
         return f"< {self.title}>"
@@ -31,12 +35,13 @@ class Product(db.Model):
 
 # User Model
 class User(db.Model):
+    """User Model class"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     admin = db.Column(db.Boolean, default=False)
-    orders = db.relationship('Order', backref='user', lazy='dynamic')
+    orders = db.relationship('Order', backref='customer', lazy='dynamic')
 
     def __repr__(self):
         """Return a string representation of the user object"""
@@ -54,12 +59,14 @@ class User(db.Model):
 
 # Order Model
 class Order(db.Model):
+    """Order Model class"""
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User', backref='orders')
+    user = db.relationship('User', backref='customer_orders')  # Changed from 'orders' to 'customer_orders'
 
     def __repr__(self):
         return f"<Order {self.id}>"
+
 
 # OrderProduct Model
 class OrderProduct(db.Model):
@@ -73,3 +80,9 @@ class OrderProduct(db.Model):
        
     def __repr__(self):
         return f"OrderProduct {self.order_id} {self.product_id}"
+    
+class Category(db.Model):
+    """Category Model class"""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    db.image = db.Column(db.String(100), nullable=False)
