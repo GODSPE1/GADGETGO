7ac7e2683455d1bc8c6c89380d1db3534d558bfc
GODSPE1 <godspy18@gmail.com>
2024-09-21 08:07:58 +0100

    added app context to manage the app instance and resources

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+    print('Database successfully created')
 
 from app import routes
