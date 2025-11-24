c516199d37e7aa03d1c2f117cab48f73982e5be9
GODSPE1 <godspowermosesdev@gmail.com>
2025-11-24 17:30:51 +0100

    update comment in Config class to clarify configuration variable usage

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     """configuration class"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key'
-    # Add other production configs here if needed
+    # Add additional configuration variables as needed
