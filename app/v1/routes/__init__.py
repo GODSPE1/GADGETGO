10ca3785e22454138d2472fc96864436ce4555a3
GODSPE1 <godspowermosesdev@gmail.com>
2025-12-01 06:03:07 +0100

    fix: add template and static folder parameters to version_one blueprint

--- app/v1/routes/__init__.py
+++ app/v1/routes/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 
-version_one = Blueprint(import_name=__name__, name="version_one")
+version_one = Blueprint(import_name=__name__, name="version_one", template_folder="templates", static_folder="static")
+# version_one = Blueprint(import_name=__name__, name="version_one", template_folder="templates", static_folder="static")
+
 
 from app.v1.routes.auth import auth
 from app.v1.routes.products import product
@@ -8,6 +10,7 @@ from app.v1.routes.category import category
 from app.v1.routes.orders import order
 from app.v1.routes.payments import payment
 from app.v1.routes.users import user
+from app.v1.routes.home import home_bp
 
 version_one.register_blueprint(auth)
 version_one.register_blueprint(product)
@@ -15,3 +18,4 @@ version_one.register_blueprint(category)
 version_one.register_blueprint(order)
 version_one.register_blueprint(payment)
 version_one.register_blueprint(user)
+version_one.register_blueprint(home_bp)
